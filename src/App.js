96a758d2e6c1e6a5a440002032caaa0293cc4d84
CapraRoyale <siege.lehman@gmail.com>
2019-11-16 13:57:00 -0800

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+  // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+  // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  // firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+};
+
+class App extends Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+        </header>
+      </div>
+    );
+  }
+}
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
