217e79bc446ba2d9da7174f3ae77ec8590507554
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-11-18 18:50:34 -0800

    updating components for firebase auth

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 const Navigation = () => (
     <div>
-        <h1>Navigation</h1>
+        <ul>
+            <li>
+                <Link to={ROUTE.SIGN_IN}>Sign In</Link>
+            </li>
+            <li>
+                <Link to={ROUTE.HOME}>Home</Link>
+            </li>
+            <li>
+                <Link to={ROUTE.LANDING}>Landing</Link>
+            </li>
+            <li>
+                <Link to={ROUTE.ACCOUT}>Account</Link>
+            </li>
+            <li>
+                <Link to={ROUTE.ADMIN}>Admin</Link>
+            </li>
+        </ul>
     </div>
 );
 
