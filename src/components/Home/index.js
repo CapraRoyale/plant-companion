aaf0264320e2fc8fca481e6a28dacc076e5bbcc5
Samuel Meyer <svmeyer@Samuels-MacBook-Pro.local>
2019-11-25 19:36:45 -0800

    moving css around and updating App/index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,17 +3,6 @@ import SignOutButton from "../SignOut"
 import styled from "styled-components"
 import "./style.css";
 
-
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-`;
-
-const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip
-`;
 const Home = () => (
     <div>
         <SignOutButton />
