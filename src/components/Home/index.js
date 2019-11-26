77f43aa814cc8bee21cd8e17044f076e51328178
CapraRoyale <siege.lehman@gmail.com>
2019-11-25 20:26:19 -0800

    Removing routes folder

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,26 +4,14 @@ import './index.css';
 function Home() {
   return (
 
-
-
-
     <div>
       <link href="components/home/css/index.css" rel="stylesheet" />
       <h1> plant and companion</h1>
-
-
-
-
 
       <form className="searchform">
         <input type="text" placeholder="Search..." name="search" />
         <button type="submit">Submit</button>
       </form>
-
-
-
-
-
 
     </div>
   )
