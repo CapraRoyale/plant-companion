c4d7063e4c7a87ed9f295e916c5aaa271c09c3f1
Scarlett <jenny_lieseke@hotmail.com>
2019-11-25 20:24:19 -0800

    Update index.js

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -21,3 +21,16 @@ const Landing = () => (
 );
 
 export default Landing;
+
+{/* card template to use for plant listing*/}
+
+{/* <Card>
+  <Card.Header>Featured</Card.Header>
+  <Card.Body>
+    <Card.Title>Special title treatment</Card.Title>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card> */}
