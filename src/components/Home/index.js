cae8d4c3664e3442e2ccc8858aad664f1d818ac8
CapraRoyale <siege.lehman@gmail.com>
2019-12-02 19:35:45 -0800

    Correct paths of resources

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './style.css';
 
 function Home () {
     return (
@@ -32,18 +32,18 @@ function Home () {
 
 export default Home;
 
-{/* card template to use for plant listing*/}
+// {/* card template to use for plant listing*/}
 
-{/* <Card>
-  <Card.Header as="h5">Featured</Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card> */}
+// {/* <Card>
+//   <Card.Header as="h5">Featured</Card.Header>
+//   <Card.Body>
+//     <Card.Title>Special title treatment</Card.Title>
+//     <Card.Text>
+//       With supporting text below as a natural lead-in to additional content.
+//     </Card.Text>
+//     <Button variant="primary">Go somewhere</Button>
+//   </Card.Body>
+// </Card> */}
 // ______________________________
 
 // collapsing button for information on plant card:
