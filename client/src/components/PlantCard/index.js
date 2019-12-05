db54ea9050477326b4b5ae782de8474137be4f66
Scarlett <jenny_lieseke@hotmail.com>
2019-12-04 21:47:30 -0800

    Update index.js

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -4,19 +4,21 @@ import axios from 'axios'
 function PlantCard({ name, desc, misc }) {
 
   return (
-    <div className="card md-12">
-      <div className='card-header'>
-        <h3>
-          <strong>
-            <i aria-hidden='true' />{name}
-          </strong>
-        </h3>
-      </div>
-      <div className='card-body'>{desc}
-        <div className='description'>
-          {misc}
+    <div class="mui--z1">
+      <div className="card md-12">
+        <div className='card-header'>
+          <h3>
+            <strong>
+              <i aria-hidden='true' />{name}
+            </strong>
+          </h3>
         </div>
+        <div className='card-body'>{desc}
+          <div className='description'>
+            {misc}
+          </div>
 
+        </div>
       </div>
     </div>
   );
