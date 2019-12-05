58d3aff3b5c95259fa85f2e98013aba5f7a73671
CapraRoyale <siege.lehman@gmail.com>
2019-12-05 09:36:52 -0800

    Merge conflicts

--- client/src/components/PlantCard/index.js
+++ client/src/components/PlantCard/index.js
@@ -5,23 +5,6 @@ import Button from '../Home/index'
 function PlantCard({ name, desc, misc }) {
 
   return (
-<<<<<<< HEAD
-    <div class="mui--z1">
-      <div className="card md-12">
-        <div className='card-header'>
-          <h3>
-            <strong>
-              <i aria-hidden='true' />{name}
-            </strong>
-          </h3>
-        </div>
-        <div className='card-body'>{desc}
-          <div className='description'>
-            {misc}
-          </div>
-
-        </div>
-=======
     <div className="card md-12">
       <div className='card-header'>
         <h3>
@@ -35,7 +18,6 @@ function PlantCard({ name, desc, misc }) {
         
         <div><strong>Growing Information: </strong>{misc}
         </div>
->>>>>>> daa6a0639db80500d4f269efa87340284bea8db6
       </div>
       {/* <Button /> */}
     </div>
